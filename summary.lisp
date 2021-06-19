09f4b1556153ef299a72f5186281930086aa9d63
ROCKTAKEY <rocktakey@gmail.com>
2021-06-20 03:04:20 +0900

    Separate to some files.

---
+++ summary.lisp
@@ -0,0 +1,33 @@
+﻿(in-package :uip)
+
+(clim:define-application-frame uip-frame-summary ()
+  ((image-plist-list :initarg :image-plist-list
+                     :accessor uip-frame-summary/image-plist-list)
+   (directory :initarg :directory
+              :accessor uip-frame-summary/directory
+              :initform #P"~/"))
+  (:menu-bar t)
+  (:panes
+   (main-image :application
+               :display-function 'summary-display-image
+               :scroll-bars :both
+               :incremental-redisplay t
+               :height 1000
+               :width 1300)
+   (hideshow (clim:with-radio-box
+                 (:type :some-of
+                  :orientation :horizontal
+                  :value-changed-callback #'hideshow-updated-callback)
+               (clim:radio-box-current-selection "F1")
+               (clim:radio-box-current-selection "F2")
+               (clim:radio-box-current-selection "F3")
+               (clim:radio-box-current-selection "F4"))))
+
+  (:layouts
+   (default (clim:vertically ()
+              (18/20 (clim:labelling (:label "Test Image"
+                                      :align-x :center
+                                      :label-alignment :top)
+                       main-image))
+              (2/20 (clim:labelling (:label "HS")
+                      hideshow))))))
